f48a8d7d50bb8a503315d618c4ecabf3557856ff
mhjp13 <jignasapatel018@gmail.com>
2025-01-23 12:27:19 -0800

    Create test.py

---
+++ Python/test.py
@@ -0,0 +1,12 @@
+# for i in range (10):
+#    print(i)
+#    if i ==5:
+#       break
+
+counter = 0
+while counter < 5:
+    print("Counter:", counter)
+    counter += 1
+    if counter == 3:
+        break;
+   
