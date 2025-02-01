983d51536686fb5fb6bd6f64a05e379dc5e30ebf
mhjp13 <jignasapatel018@gmail.com>
2025-02-01 10:37:34 -0800

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -1,12 +1,2 @@
-# for i in range (10):
-#    print(i)
-#    if i ==5:
-#       break
-
-counter = 0
-while counter < 5:
-    print("Counter:", counter)
-    counter += 1
-    if counter == 5:
-        break;
-   
+for i in range(10):
+    print(i)
